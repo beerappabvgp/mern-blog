a62e1ec99b8672a09cfdfd6b3666664c4a74ca62
beerappabvgp <beerappabharathb@gmail.com>
2024-01-21 19:34:36 +0530

    created the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -38,7 +38,7 @@ export default function Header() {
         </Button>
         {/* signin button */}
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
         </Link>
         {/* hamburger button */}
         <Navbar.Toggle/>
