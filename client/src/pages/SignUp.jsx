1e400f6e9e1edff4d121a447d9cb60969c592ada
beerappabvgp <beerappabharathb@gmail.com>
2024-01-21 22:16:26 +0530

    completed signup page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,63 @@
-
+import { Button, Label, TextInput } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* for left side */}
+        <div className="flex-1">
+          <Link
+            to="/"
+            className="sm:text-xl font-bold dark:text-white text-4xl"
+          >
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              People's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5 font-semibold">
+            Welcome to People's blog, your go-to destination for insightful and
+            engaging content! Whether you're a passionate writer looking to
+            share your thoughts or a curious reader eager to discover new
+            perspectives, Blog Haven is the perfect platform for you. Explore a
+            diverse range of topics, from technology and lifestyle to travel and
+            personal development. Our user-friendly interface makes it easy to
+            navigate through an extensive collection of articles crafted by
+            talented writers from around the globe.
+          </p>
+        </div>
+        {/* for right side */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            {/* first field */}
+            <div className="">
+              <Label value="Your Username" />
+              <TextInput type="text" placeholder="Username" id="username"/>
+            </div>
+            {/* second field */}
+            <div className="">
+              <Label value="Your Email" />
+              <TextInput type="text" placeholder="name@blog.com" id="email"/>
+            </div>
+            {/* third field */}
+            <div className="">
+              <Label value="Your Password" />
+              <TextInput type="password" placeholder="Password" id="password"/>
+            </div>
+            {/* button */}
+            <Button gradientDuoTone='purpleToPink' type="submit">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to='/sign-in' className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
